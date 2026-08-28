eb476d910a0bb818dd08c7cc697aec50c01a1c69
THARA HUOKAING <tharahuokaing@gmail.com>
2026-08-28 15:48:50 +0700

    Add new user 'dalin' to login credentials

--- js/login.js
+++ js/login.js
@@ -29,6 +29,7 @@
         { username: "do", password: "do", role: "VIP Customer", requires2FA: false },
         { username: "sokrachana", password: "sokrachana", role: "VIP Customer", requires2FA: false },
         { username: "sokkhemera", password: "sokkhemera", role: "VIP Customer", requires2FA: false },
+        { username: "dalin", password: "dalin", role: "VIP Customer", requires2FA: false },
         { username: "dom", password: "dom", role: "VIP Customer", requires2FA: false },
         { username: "kimmuy", password: "kimmuy", role: "Secretary", requires2FA: false },
         { username: "kimmuy", password: "kimmuy", role: "Chief of Customer Service", requires2FA: false }, 
